0ff3df99ffd05f9451663ae38610a9dad81dbf73
Abdul8446 <farhanfarhan832@gmail.com>
2023-05-28 01:50:35 +0530

    cors change again

--- server/app.js
+++ server/app.js
@@ -1,51 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors=require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
+var indexRouter = require("./routes/index");
+var adminRouter = require("./routes/admin");
 var app = express();
 
-// view engine setup   
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
-app.use(express.json());   
+app.use(logger("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: ['http://localhost:3000','http://localhost:3001','http://football-tank.site','http://www.football-tank.site','https://www.football-tank.site','http://football-tank.site'],
-  methods: ["GET","POST","PUT","DELETE"],
-  credentials:true
-}))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://football-tank.site",
+      "http://www.football-tank.site",
+      "https://www.football-tank.site",
+      "http://football-tank.site",
+      "http://football-tank.site/api",
+      "http://www.football-tank.site/api",
+      "https://www.football-tank.site/api",
+      "http://football-tank.site/api"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
+app.use("/", indexRouter);
+app.use("/admin", adminRouter);
 
-app.use('/', indexRouter);
-app.use('/admin', adminRouter);
-
-
-// catch 404 and forward to error handler         
-app.use(function(req, res, next) {
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-     
-
 module.exports = app;
-   
